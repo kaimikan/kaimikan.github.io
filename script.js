a548ab83aaa85abad3077177ff8960bdbbd33085
kaimikan <oihic@protonmail.com>
2024-08-03 20:27:24 +0300

    Fix CSS Theme Flickering & Add Scrollbar Resizing

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // NAV RESIZE BURGER MENU
-  document.getElementById('burger-icon').addEventListener('click', function () {
-    this.classList.toggle('open');
-    document.getElementById('nav-menu').classList.toggle('open');
-  });
-
-  // CHANGE CV LINK ON THEME TOGGLE
-  const themeLink = document.getElementById('theme-link');
-
-  function updateLinkHref() {
-    const theme = document.documentElement.getAttribute('data-theme');
-    if (theme === 'light') {
-      themeLink.setAttribute('href', 'assets/resume.pdf');
-    } else {
-      themeLink.setAttribute('href', 'assets/resume_dark.pdf');
-    }
-  }
-
-  // Initially set the correct link href
-  updateLinkHref();
-
   // DARK & LIGHT THEME TOGGLE
   const prefersDarkScheme = window.matchMedia(
     '(prefers-color-scheme: dark)'
@@ -29,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.documentElement.setAttribute('data-theme', initialTheme);
 
   const toggleButton = document.getElementById('theme-toggle');
-  const currentTheme = localStorage.getItem('theme') || 'light';
+  const currentTheme = localStorage.getItem('theme') || 'dark';
   document.documentElement.setAttribute('data-theme', currentTheme);
 
   toggleButton.addEventListener('click', () => {
@@ -39,7 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
         : 'light';
     document.documentElement.setAttribute('data-theme', newTheme);
     // allow updating the link dynamically with the theme button
-    updateLinkHref();
+    updateLinkHref(newTheme);
     localStorage.setItem('theme', newTheme);
   });
+
+  // NAV RESIZE BURGER MENU
+  document.getElementById('burger-icon').addEventListener('click', function () {
+    this.classList.toggle('open');
+    document.getElementById('nav-menu').classList.toggle('open');
+  });
+
+  // CHANGE CV LINK ON THEME TOGGLE
+  const themeLink = document.getElementById('theme-link');
+
+  function updateLinkHref(theme) {
+    if (theme === 'light') {
+      themeLink.setAttribute('href', 'assets/resume_light.pdf');
+    } else {
+      themeLink.setAttribute('href', 'assets/resume.pdf');
+    }
+  }
+
+  updateLinkHref(currentTheme);
+
+  // DYNAMICALLY ADJUST BODY WIDTH WHEN SCROLLBAR POPS UP
+  function adjustPaddingForScrollbar() {
+    const hasVerticalScrollbar =
+      window.innerWidth > document.documentElement.clientWidth;
+    document.body.style.paddingRight = hasVerticalScrollbar ? '0' : '17px'; // 17px is a common scrollbar width
+  }
+
+  adjustPaddingForScrollbar();
+  window.addEventListener('resize', adjustPaddingForScrollbar);
 });
